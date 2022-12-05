96f17a503bf37bd437ec8c5382955542bdcf12c0
letMarchezi <leticiabossatto@gmail.com>
2022-12-05 20:47:28 -0300

    Adding PPD - EP1 assignment

---
+++ PPD/EP1/job.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -J mmul                     # Job name
+#SBATCH -p fast                     # Job partition
+#SBATCH -n 1                        # Number of processes
+#SBATCH -t 01:30:00                 # Run time (hh:mm:ss)
+#SBATCH --cpus-per-task=40          # Number of CPUs per process
+#SBATCH --output=%x.%j.out          # Name of stdout output file
+#SBATCH --error=%x.%j.err           # Name of stderr output file
+
+echo "*** SEQUENTIAL ***"
+srun singularity run container.sif pi_seq 1000000000
+
+echo ""
+echo "*** PTHREAD ***"
+for i in 1 2 5 10 20 40
+do
+	echo "execucao com $i threads:"
+	srun singularity run container.sif pi_pth 1000000000 $i	
+done
+echo ""
+
+echo "*** OPENMP ***"
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+
+for i in 1 2 5 10 20 40
+do
+	echo "execucao com $i threads:"
+	export OMP_NUM_THREADSi=i
+	srun singularity run container.sif pi_omp 1000000000
+done
