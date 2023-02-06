38cfc46b61813e5d7e2e5aa832a58204d69b90b6
letMarchezi <leticiabossatto@gmail.com>
2023-02-05 21:04:25 -0300

    Update Lista 4 PAA

--- PAA/lista4/questao9.py
+++ PAA/lista4/questao9.py
@@ -1,22 +1,13 @@
-def cut_aux(p, n):
-    r = [-1]*(n+1)
-    return cutRod(p, n, r)
-    
 
-def cutRod(p, n, r):
-    if r[n] >= 0:
-        return r[n]
-    elif not n:
-        q = 0
-    else:
-        q = -1
-        for i in range(n):
-            a = p[i]+ cutRod(p, n-1, r)
-            q = max(q, a)
-            print(f"q = max({q}, {a})")
-    r[n] = q
-    return q
+def cutRodB(p, n):
+    r = [0]*(n+1)
+    for j in range(1,n):
+        q = -1.8e308
+        for i in range(1,j):
+            q = max(q, float(p[i]+r[j-i]))
+        r[j] = q
+    return r[n]
 
 
-
-print(cut_aux([2, 6,9,10,12,16], 6))
+a = [2, 6,9,10,12,16]
+cutRodB(a,6)
