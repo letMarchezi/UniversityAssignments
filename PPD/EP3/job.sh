1e28f616863d91ab5b75216c3e2a76567ad3be31
letMarchezi <leticiabossatto@gmail.com>
2023-02-08 21:36:47 -0300

    Fixing EP3 - PPD files

--- PPD/EP3/job.sh
+++ PPD/EP3/job.sh
@@ -6,15 +6,20 @@
 #SBATCH --cpus-per-task=40          # Number of CPUs per process
 #SBATCH --output=%x.%j.out          # Name of stdout output file
 #SBATCH --error=%x.%j.err           # Name of stderr output file
-
+lscpu
 
 echo "*** SEQUENTIAL LAPLACE EQUATION GRID 1000X1000 ***"
 srun singularity run container.sif laplace_seq_it 1000
 echo " "
-echo "*** PTHREAD LAPLACE EQUATION grid 1000x1000 ***"
+echo "*** OPENMP LAPLACE EQUATION grid 1000x1000 ***"
 
 for j in 1 2 5 10 20 40
 do
 	export OMP_NUM_THREADS=$j
+
+	echo "OPENMP PARALLEL LAPLACE EQUATION WITH $j THREADS"
 	srun singularity run container.sif laplace_omp_it 1000
+	echo "----------------------------------------------"
+	echo " "
+	
 done
